1a9c1af17de6cb27104bb831f6b3b146d88b9970
Ricardo Bona <ricardoabbona@gmail.com>
2024-09-03 23:07:11 -0300

    Versão final

--- mEP7.py
+++ mEP7.py
@@ -1,79 +1,103 @@
-def notasValidas(n,notas = [], i = 0):
-
+def notas(n, carteira = [], i = 0):
     if i < n:
-        notas += [int(input())] #lista que registra as notas
-        return notasValidas(n,notas, i+1)
+        item = int(input())
+        if item not in carteira:
+            carteira += [item]
+        
+        return notas(n,carteira, i +1)
+
+    return carteira
+
+
+
+def deposito(notasV, carteiraD = []):
     
-    return notas
-
-def depositos(n,notas, carteiraDigital,i = 0,f = 0, r = 0):
-   
-    if r == 2 or r == 0:
-        i = int(input()) #ele que comandará a recursão da função
-
-    if i < 0:
-        return carteiraDigital
+    if carteiraD == []:
+        carteiraD = [0]*len(notasV)
+        
     
-    if f < len(notas):
+    n = int(input())
 
-        if i == notas[f]:
-            carteiraDigital[f] += 1
+    if n >= 0:
+        if n in notasV:
+            indice = notasV.index(n)
+            carteiraD[indice] += 1
 
-        return depositos(n,notas,carteiraDigital,i,f +1, r = 1)
+        return deposito(notasV,carteiraD)
     
-    return depositos(n, notas, carteiraDigital,i, f = 0, r = 2)
 
-def saldo(n,notas, carteiraDigital, saldoD = 0, i = 0):
-    if i < n:
-        saldoD += notas[i] * carteiraDigital[i]
-        return saldo(n,notas,carteiraDigital, saldoD, i+1)
+    return carteiraD
+
+def saldo(notasV,carteiraD,saldo_d = 0, i = 0):
+
+    if i < len(notasV) and (len(notasV) == len(carteiraD)):
+
+        saldo_d += notasV[i]*carteiraD[i]
+
+        return saldo(notasV,carteiraD,saldo_d, i + 1)
     
-    return saldoD
+    return saldo_d
 
-def estoqueN(n, notas, carteiraD, i = 0):
-    if i < n:
-        print(f"${notas[i]}: {carteiraD[i]}")
-        return estoqueN(n,notas,carteiraD, i + 1)
-
-def printSaldoEstoque(n,notas,listaSE, i = 0):
-    if i < len(listaSE):
-        if listaSE[i][0] == "s":
-            print(f"Saldo: ${listaSE[i][1]}")
-        else:
-            estoqueN(n,notas,listaSE[i][1])
-
-        return printSaldoEstoque(n,notas,listaSE, i + 1)
+def estoqueNotas(notasV,carteiraD, i = 0):
     
-def menu(n,notas,carteiraD=0, saldoD=0, listaSaldEst = [],tuplaE = ()):
+    if carteiraD == []:
+        carteiraD = [0]*len(notasV)
+
+    if i < len(notasV):
+
+        nota = notasV[i]
+        quantidade = carteiraD[i]
+
+        print(f"${nota}: {quantidade}")
     
-    if saldoD == 0:
-        carteiraD = [0]*len(notas)
+        return estoqueNotas(notasV,carteiraD, i + 1)
+    
+        
+def impressao(comando,notasV,i = 0):
+    if i < len(comando):
+
+        if comando[i][0] == "s":
+            saldo = comando[i][1]
+            print(f"Saldo: ${saldo}")
+        
+        if comando[i][0] == "e":
+            carteiradigital = comando[i][1]
+            estoqueNotas(notasV,carteiradigital)
+    
+        return impressao(comando,notasV, i+1)
+
+
+
+def menu(notasV,carteira_digital = [], saldo_digital = 0,comando_digital = []):
 
     opcao = input()
 
+
     if opcao == "DEPOSITO":
-        carteiraD = depositos(n,notas,carteiraD)
-        saldoD = saldo(n,notas,carteiraD)
-        carteiraD2 = carteiraD + []
-        tuplaE = ("e",carteiraD2)
+        carteira_digital = deposito(notasV,carteira_digital + [])
     
     if opcao == "SALDO":
+        saldo_digital = saldo(notasV,carteira_digital)
 
-        listaSaldEst += [("s",saldoD)]
+        comando_digital += [("s",saldo_digital)] 
     
     if opcao == "ESTOQUENOTAS":
-        listaSaldEst += [tuplaE]
-
+        comando_digital += [("e",carteira_digital)] + []
+    
     if opcao == "FIM":
-        printSaldoEstoque(n,notas,listaSaldEst)
+        impressao(comando_digital,notasV)
         return None
 
-    return menu(n, notas, carteiraD, saldoD,listaSaldEst,tuplaE)
     
-def main():
-    n = int(input()) #representa o número de notas válidas do sistema
-    notas = notasValidas(n)
+    return menu(notasV, carteira_digital,saldo_digital)
 
-    menu(n,notas)
-   
+def main():
+
+    numero_notas_validas = int(input())
+
+    notas_validas = notas(numero_notas_validas)
+
+    menu(notas_validas)
+
+
 main()
